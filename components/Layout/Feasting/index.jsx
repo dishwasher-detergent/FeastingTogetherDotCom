1173af4083034c012b3c1b40f21058cde9d3be5a
Kenny Bass <kenny_bass@outlook.com>
2022-04-13 16:59:34 -0500

    Update index.jsx

--- components/Layout/Feasting/index.jsx
+++ components/Layout/Feasting/index.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useRouter } from 'next/router'
+import { useSelector, useDispatch } from 'react-redux'
+
 const FeastingLayout = ({ children }) =>
 {
+	const connection = useSelector((state) => state.connection)
+	const router = useRouter()
+
+	useEffect(() => {
+		if(!connection) router.push("/")
+	},[]);
+
 	return (
 		<main className="w-full min-h-screen flex flex-col items-center justify-center bg-slate-50">
 			{children}
