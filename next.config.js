da94215d375f4122461d4f3674bc2fcf7b1f932a
dishwasher-detergent <kenny_bass@outlook.com>
2022-04-13 22:53:05 -0500

    remove env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    REACT_APP_SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTEyNjYyOCwiZXhwIjoxOTQ0NzAyNjI4fQ.6Eq6xevtBU923akoJYYIXZsm68p1HOStQTClGaoPrWM"
+    REACT_APP_SUPABASE_KEY: process.env.REACT_APP_SUPABASE_KEY
   }
 }
 
