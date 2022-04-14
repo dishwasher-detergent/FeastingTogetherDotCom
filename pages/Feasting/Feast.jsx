4cab2dd812433b1cbee67525ef8e525efc71fc1e
dishwasher-detergent <kenny_bass@outlook.com>
2022-04-13 23:00:28 -0500

    strokeLinecap

--- pages/Feasting/Feast.jsx
+++ pages/Feasting/Feast.jsx
@@ -65,7 +65,7 @@ const Feast = () =>
 						<button className='button success w-full' onClick={() => setLikes(true)}>
 							Yes Please!
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path stroke-linecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                             </svg>
 						</button>
 					</div>
