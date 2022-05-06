ababc584ea37c9622e787b86a0d4d6032ccd16fd
Kenny Bass <kenny_bass@outlook.com>
2022-05-06 13:43:28 -0500

    fix subtext

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <section className="mt-12 py-8 md:py-24 rounded-md">
           <div className="flex flex-col items-center justify-center pb-12 md:pb-24">
             <h1 className="magic pb-12 text-6xl md:text-7xl lg:text-8xl font-bold">Feasting<br />Together</h1>
-            <p className="text-lg md:text-2xl">Helping you and your buddies find the perfect place to eat!</p>
+            <p className="text-lg md:text-xl">Helping you and your buddies find the perfect place to eat!</p>
           </div>
           <div className="flex flex-col-reverse md:flex-row items-center justify-center gap-1">
               <Link href={'/Feasting/Join'}>
